a719bbb8d3dd07b898b527bafec62805c1b0c14e
Kudos4U <42659997+BriBuff@users.noreply.github.com>
2018-11-10 14:17:35 -0700

    buttons

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,43 +3,47 @@ var topics = ["Anime", "Yuri on Ice!", "Full Metal Alchemist", "DragonBall Z", "
 
 // Pulling gifs
 
-for (var i= 0; i < topics.length; i++) {
-    var topicBttn = $("<button>");
-    topicBttn.addClass(".bttn");
-    topicBttn.attr("data", topics[i]);
-    topicBttn.text(topics[i]);
-    $("#animeBttns").append(topicBttn);
+// Making buttons/new buttons
+function newAnimeBttns() {
+    $("#animeBttns").empty();
+    for (var i= 0; i < topics.length; i++) {
+        var topicBttn = $("<button>");
+        topicBttn.addClass("bttn");
+        topicBttn.attr("data", topics[i]);
+        topicBttn.text(topics[i]);
+        $("#animeBttns").append(topicBttn);
+    } 
 }
 
-var topic = 
-
-// Displaying gifs
-function displayGifs() {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=sTY8u4svT7q5V5kmbOFxWn7S1SyfRB3b&tag=" + topic + "&limit=10";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
+    $("#add-anime").on("click", function(event) {
+        event.preventDefault();
+        var anime = $("#anime-input").val().trim();
+        topics.push(anime);
+        newAnimeBttns();
     });
 
-};
+newAnimeBttns();
 
-// Making new buttons
-function animeBttns() {
-    $("#animeBttns").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var bttn = $("<button>");
-        bttn.addClass("anime-button");
-        bttn.attr("data-name", topics[i]);
-        bttn.text(topics[i]);
-        $("animeBttns").append(bttn);
-    }
-};
+// Displaying gifs
+// function displayGifs() {
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=sTY8u4svT7q5V5kmbOFxWn7S1SyfRB3b&tag=" + topic + "&limit=10";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//     });
+
+// };
+
 
 
 // click function- display gifs, limit rating, (call functions above?)
-$("#add-anime").on("click", function() {
-    displayGifs();
-    animeBttns();
-});
+// $("#add-anime").on("click", function(event) {
+//     event.preventDefault();
+//     var anime = $("#animeBttns").val().trim();
+//     topics.push(anime);
+//     newAnimeBttns();
+// });
+
+// Calling newAnimeBttns() to show the initial list
